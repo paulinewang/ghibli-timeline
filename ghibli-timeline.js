9bc334f0445756125ea5da980513b4f740d58a2f
Pauline Wang <wbl.pauline@gmail.com>
2020-12-01 22:48:27 +0100

    Add proportional spacing to timeline

--- ghibli-timeline.js
+++ ghibli-timeline.js
@@ -46,4 +46,22 @@ function sideScroll(element, direction, speed, distance, step) {
   }, speed);
 }
 
-// calculate relative spacing between the year timestamps
+(function relativeTimelineSpacing() {
+  // 1. List all years in an array
+  const years = document.querySelectorAll(".timeline-item .timeline-year");
+  let allYears = [];
+  years.forEach((year) => {
+    allYears.push(year.textContent);
+  });
+  for (i = 0; i < years.length; i++) {
+    // 3. For every year, calculate the difference/distance with the next year
+    // One year difference is 60px
+    let diff = years[i + 1].textContent - years[i].textContent;
+    if (diff === 0) {
+      diff = 1;
+    }
+    const margin = diff * 60;
+    // 4. Position the item by converting the relative value into margin-right
+    years[i].style.marginRight = `${margin}px`;
+  }
+})();
